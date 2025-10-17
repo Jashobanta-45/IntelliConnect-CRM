e55549dc2ef925b51342c2ebf3b00b4f2bf7439d
Jashobanta Behera <jashobantabehera2022@gmail.com>
2025-10-17 12:42:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9348459508-bae6e",
+  "appId": "1:244872727833:web:a33804b3d278c89151c988",
+  "apiKey": "AIzaSyBEweQST4FT0fhNwja6qZTGmSEwikcg0qE",
+  "authDomain": "studio-9348459508-bae6e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "244872727833"
+};
